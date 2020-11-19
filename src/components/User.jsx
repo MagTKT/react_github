7dad6b509559d75ae47c0320eb39ec5ecd620e56
magali gosselin <magali@wertkt.com>
2020-11-19 15:52:08 +0100

    changes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import Repos from './Repos';
 
 class User extends React.Component {
     constructor() {
@@ -20,8 +20,9 @@ class User extends React.Component {
         );
     }
 
-    render() {
 
+    render() {
+< Repos/>
         if (!this.state.user) {
             return (<div className="user-page">LOADING...</div>);
         }
@@ -35,10 +36,12 @@ class User extends React.Component {
                         <img className="user-info__avatar" src={user.avatar_url} alt={`${user.login} avatar`}/>
                         <h2 className="user-info__title">{user.login} ({user.name})</h2>
                         <p className="user-info__bio">{user.bio}</p>
+                        
                     </Link>
                 </div>
 
             </div>
+            
         )
     }
 };
--- src/index.js
+++ src/index.js
@@ -7,16 +7,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/App';
 import Search from './components/Search';
 import User from './components/User';
-import Repos from './components/Repos';
 
 const routes = (
     <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Search}/>
-            <Route path="user/:username" component={User} > 
-                <Route path="repos" component={Repos} />
-            </Route>
-        </Route>
+        <Route path="/" component={Search}/>
+        <Route path="user/:username" component={User} /> 
     </Router>
 );
 
